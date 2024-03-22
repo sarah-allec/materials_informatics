c792806a727f6f7e2114be60aa47a2384bd650dd
Sarah Allec <sarah.allec@gmail.com>
2024-03-21 21:24:07 -0600

    updated menu

--- streamlit/pages/Active_Learning.py
+++ streamlit/pages/Active_Learning.py
@@ -1,4 +1,6 @@
+from menu import custom_menu
 import streamlit as st
 
 st.write("# Active Learning 🏃🏽‍♀️")
 
+custom_menu()
--- streamlit/pages/Physics_Informed_Learning.py
+++ streamlit/pages/Physics_Informed_Learning.py
@@ -1,4 +1,6 @@
+from menu import custom_menu
 import streamlit as st
 
 st.write("# Physics-Informed Learning ⚛️")
 
+custom_menu()
