421a0f5eb297db3f674f9e67352a8f3c2fb8b046
Sarah Allec <sarah.allec@gmail.com>
2024-03-21 21:32:58 -0600

    deleted menu

--- menu.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def unauthenticated_menu():
-    # Show a navigation menu for unauthenticated users
-    st.sidebar.page_link("Materials_Informatics.py", label="Home 🏠")
-    st.sidebar.page_link("pages/Transfer_Learning.py", label="Transfer Learning 🤝")
-    st.sidebar.page_link("pages/Active_Learning.py", label="Active Learning 🏃🏽‍♀️")
-    st.sidebar.page_link("pages/Physics_Informed_Learning.py", label="Physics-Informed Learning ⚛️")
